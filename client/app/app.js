c366bb6a9e4961364ba69fa036cdb446ed080bac
jimjea <jimjea@gmail.com>
2014-11-25 18:38:48 -0800

    changed text

--- client/app/app.js
+++ client/app/app.js
@@ -12,7 +12,7 @@ angular.module('speed-read',[
 .factory('Main', function() {
   var testText = function() {
     return {
-      text: 'George Washington (February 22, 1732 [O.S. February 11, 1731][Note 1][Note 2] – December 14, 1799) was the first President of the United States (1789–1797), the Commander-in-Chief of the Continental Army during the American Revolutionary War, and one of the Founding Fathers of the United States.[3] He presided over the convention that drafted the United States Constitution, which replaced the Articles of Confederation and remains the supreme law of the land.'
+      text: 'George Washington was born on February 22, 1732, in Westmoreland County, Virginia. Washington served as a general and commander-in-chief of the colonial armies during the American Revolution, and later became the first president of the United States, serving from 1789 to 1797. He died on December 14, 1799, in Mount Vernon, Virginia.'
     };
   };
 
