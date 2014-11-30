6b878929f18ec48f9feb28f98de279f2f0cd5865
jimjea <jimjea@gmail.com>
2014-11-29 17:31:27 -0800

    created directive for whole text highlighting

--- client/app/whole-text/wholetext.js
+++ client/app/whole-text/wholetext.js
@@ -10,5 +10,24 @@ angular.module('speed-read.wholetext', ['ui.router'])
 })
 
 .controller('WholeTextController', function($scope, Main) {
-  $scope.text = Main.testText().text;
+  $scope.text = Main.testText().text.split(' ');
+})
+
+.directive('textScanner', function($interval) {
+  function highlight(scope, element, attrs) {
+    console.log(element[0].textContent)
+    // need to access the innerhtml (currently returning {{text}})
+    // split the text
+    // iterate through text
+      // toggle classes to highlight words in text
+
+  }
+
+  return {
+    // scope: {
+    //   wholeText: '='
+    // },
+    // templateUrl: 'app/whole-text/highlight.html',
+    highlight: highlight
+  }
 })
