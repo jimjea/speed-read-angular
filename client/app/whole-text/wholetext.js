acf346f615c447cf392c05735e8d890e5748984b
jimjea <jimjea@gmail.com>
2014-12-03 09:54:49 -0800

    moved all logic from controller into directive

--- client/app/whole-text/wholetext.js
+++ client/app/whole-text/wholetext.js
@@ -10,40 +10,33 @@ angular.module('speed-read.wholetext', ['ui.router'])
 })
 
 .controller('WholeTextController', function($scope, Main, $timeout) {
-  var text = Main.testText().text.split(' '); // pull back text data from factory
-  var index = 0;
-  var fast = 1;
-  var tail = -1;
-  var length = text.length;
-  var textSpan = [];
-  for (var i = 0; i < length; i++) { // turning all words into a span tag, and pushing it to an array
-    textSpan.push(angular.element('<span class=' + i + '>').text(text[i] + ' '));
-  }
 
-  $scope.text = textSpan;
-
-  $scope.highlighter = function() { // highlighter function, will move into directive
-    if (index < length) {
-      $('.' + index).css('background-color', 'yellow');
-      $('.' + fast).css('background-color', 'yellow');
-      $('.' + tail).css('background-color', 'white');
-      index++;
-      tail++;
-      fast++;
-      $timeout($scope.highlighter, 270);
-    }
-  }
+  $scope.text = Main.testText().text; // pull text form factory. The text variable will be passed into the directive
 
 })
 
-.directive('textScanner', function($timeout) {
+.directive('textHighlighter', function($timeout) {
   function link(scope, element, attrs) {
     var text;
 
     function highlight() { // append all words to the directive
-      for (var i = 0; i < text.length; i++) {
-        element.append(text[i]);
+      var split = text.split(' ');
+      var index = 0;
+      var tail = -1;
+      var length = split.length;
+      for (var i = 0; i < length; i++) { // turning all words into a span tag, and appending them to the dom
+        element.append(angular.element('<span class=' + i + '>').text(split[i] + ' '));
       }
+      var highlighter = function() {
+        if (index < length) {
+          $('.' + index).css('background-color', 'yellow');
+          $('.' + tail).css('background-color', 'white');
+          index++;
+          tail++;
+          $timeout(highlighter, 270);
+        }
+      }
+      highlighter()
     };
 
     scope.$watch(attrs.textScanner, function(value) {
